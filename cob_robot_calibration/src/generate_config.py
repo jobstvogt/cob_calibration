03252bb8783b4d813ae8f48629e1e117f505c0ca
Bennet Jobstvogt <stefan.kaiser@dai-labor.de>
2014-11-11 11:29:25 +0100

    calibration config

--- cob_robot_calibration/src/generate_config.py
+++ cob_robot_calibration/src/generate_config.py
@@ -258,6 +258,7 @@ def __main__():
     # sss.move("head", "back")
     sss = simple_script_server()
     sss.move("head", "back")
+    sss.move("torso", "home")
     minimal_system = rospy.get_param('minimal_system', None)
     sensors_path = rospy.get_param('sensors', None)
     output_system = rospy.get_param('output_system', None)
